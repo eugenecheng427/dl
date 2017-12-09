e71374ad9aaf6591b579e047aec9289e6ad04bc9
Eugene Cheng <eugene427@gmail.com>
2017-12-09 22:01:22 +0800

    修改路徑
    整理index.js

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 
 
-// var server = require("./server");
-// var router = require("./router");
-
-// server.start(router.route);
 var cool = require('cool-ascii-faces');
 var express = require('express');
 var bodyParser = require("body-parser");
@@ -15,26 +11,12 @@ var db = require('node-localdb');
 var orderAllDb = db('data/order.json');
 var updateJson = require('update-json');
 
-var multer = require('multer'); //檔案上傳囉！！
-var upload = multer({
-    dest: 'uploads/'
-});  //dest: 你的檔案儲存路徑，這裡是設定在根目錄底下的uploads資料夾
-
-// assert = require('assert');
-
-// var db = new Engine.Db('/data', {});
-// var collection = db.collection("orderAll");
 
 
-// Connect to MongoDB
-// var mongo = require('mongodb');
-// var mongoose = require('mongoose');
-// mongoose.connect('mongodb://eugenecheng:eu427724@ds129146.mlab.com:29146/dlivejerseys');
-// var db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'connection error:'));
-// db.once('open', function callback () {
-//   console.log("Database Connected.");
-// });
+
+
+
+
 
 
 
@@ -44,7 +26,6 @@ var upload = multer({
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.static(__dirname + '/uploads/'));
-// app.use(express.static(__dirname + '/public/'));
 
 
 app.get('/cool', function (request, response) {
@@ -82,17 +63,13 @@ app.get('/demo.html', function (req, res) {
     });
 });
 
-app.get('/orderpage', function (req, res) {
-    // console.log('orderpage');
-
-
+app.get('/orderpage', function (req, res) {       
     res.sendFile(__dirname + '/orderpage.html', function (err) {
         if (err) res.sendStatus(404);
     });
 });
 
-app.post('/orderpage', function (req, res, next) {
-    // console.log('orderpage');
+app.post('/orderpage', function (req, res, next) {  
     var dataPath = __dirname + '/data/order.json';
     var ordersData = "";
     fs.readFile(dataPath, function (err, content) {
@@ -115,8 +92,6 @@ app.post('/orderpage', function (req, res, next) {
     // });
 });
 app.get('/showorder', function (req, res) {
-        // console.log('orderpage');
-
 
     res.sendFile(__dirname + '/showorder.html', function (err) {
         if (err) res.sendStatus(404);
@@ -128,34 +103,28 @@ app.post('/showorder', function (req, res, next) {
      var ordersData = "";
      fs.readFile(dataPath, function (err, content) {
 
-        //  console.log('content ='+content);
-        //  console.log('content.length ='+content.length);
-        //  var ordersData = content;
          var ordersData = content;
 
                      res.writeHead(200, {
              "Content-Type": "application/json",
              "Content-Length": ordersData.length
-         });            
-        //  console.log('returndata ');            
+         });                       
          res.end(ordersData);
      });
      
  });
 
 app.get(/(.*)\.(jpg|gif|png|ico|css|js|txt|svg)/i, function (req, res) {
-    // console.log('==>'+__dirname + req.params[0] + "." + req.params[1]);
     res.sendFile(__dirname + "/" + req.params[0] + "." + req.params[1], function (err) {
         // console.log('err='+err);
-        // if (err) res.sendStatus(404);
-        if (err) res.status(err.staus).end();
+        if (err) res.sendStatus(404);
+        // if (err) res.status(err.staus).end();
     });
 });
 
 
 
 app.post('/uploadfile', function (req, res) {
-    // console.log('come to save');  
 
 
     var data_url = req.body.imgBase64;
@@ -181,17 +150,12 @@ app.post('/uploadfile', function (req, res) {
     var orderName = username;
     var orderEmail = email;
     var userphone = phone;
-    var imageFile = __dirname + '\\uploads' + '\\' + phone + '.jpg';
-    // var imageFile = '/uploads' + '/' + phone + '.jpg';
+    // var imageFile = __dirname + '\\uploads' + '\\' + phone + '.jpg';
+    var imageFile = './uploads' + '/' + phone + '.jpg';
     var orderobj = {};
     var orderDetail = [];
     var onumber = orderNumber;
-    var detail = {
-        // playerName,
-        // playerNumber,
-        // shirtSize,
-        // paintSize
-    };
+  
 
     // var cart ={};
     orderobj = {
@@ -214,38 +178,23 @@ app.post('/uploadfile', function (req, res) {
 
 
     var dataEmpty = false;
-    var dataPath = __dirname + '/data/order.json';
+    // var dataPath = __dirname + '/data/order.json';
+    var dataPath =  './data/order.json';
 
 
-    fs.writeFile(__dirname+'\\uploads' + '\\' + phone + '.jpg', buffer, function (err) {
-        // console.log('[S,M,L,XL,28,30,32,34] ' + '\n' + '[' + shirtS + ',' + shirtM + ',' + shirtL + ',' + shirtXL + ',' + paint28 + ',' + paint30 + ',' + paint32 + ',' + paint34 + ']');
-        // res.send('success');
+    fs.writeFile('./uploads' + '/' + phone + '.jpg', buffer, function (err) {
+        
         //資料存資料庫
-        // console.log('readjsonouttttttt' );
         fs.readFile(dataPath, function (err, content) {
-            // console.log('readjsoninnnnnnnn' );
             if (err) throw err;
             var parseJson = JSON.parse(content);
-            // console.log('orderNumber =' +orderNumber);
-            // console.log('parseJson =' +parseJson);
-            // console.log('parseJson.length =' +parseJson.length);
-            // console.log('parseJson[0].oobj.orderName =' +parseJson[0].oobj.imageFile);
 
             if (parseJson.length > 0) {
                 for (i = 0; i <= parseJson.length; i++) {
-                    // console.log('parseJson['+i+']' );
-                    // console.log('parseJson.length ['+parseJson.length +']' );
                     if (i === parseJson.length) {
-                        // console.log('break' );
-                        // console.log('parseJson['+i+']' );
                         parseJson.push({ onumber: orderNumber, oobj: orderobj });
                         break;
                     }
-                    // if(i === parseJson.length){
-                    //     console.log('i == parseJson.length parseJson['+i+']' ); 
-                    //     parseJson.push({onumber:orderNumber, oobj: orderobj});
-                    //     // continue;
-                    // }else 
                     if (parseJson[i].onumber == orderNumber) {
 
                         var orders = parseJson;
@@ -258,93 +207,29 @@ app.post('/uploadfile', function (req, res) {
                         }
                         //  console.log('Json =' +orderNumber);
                         parseJson.push({ onumber: orderNumber, oobj: orderobj });
-                        // console.log('parseJson[' + i + '] =' + parseJson[i].onumber);
-                        // console.log('Json =' +orderNumber);
-                        // parseJson[i].oobj = orderobj;
-                        // parseJson[i].onumber = orderNumber;
-                        // parseJson[i].oobj = orderobj;
-                        // var inputdata ={onumber:orderNumber,oobj:orderobj};
-                        // inputdata = {onumber:orderNumber, oobj: orderobj};
-                        // console.log('parseJson.oobj.orderName[' + i + '] =' + parseJson[i].oobj.orderName);
-                        // parseJson.updateJson({onumber: parseJson[i].onumber, oobj: parseJson[i].oobj});
-                        // parseJson.push({ onumber: parseJson[i].onumber, oobj: parseJson[i].oobj });
-                        // updateJson(dataPath, parseJson[i], function (error) {
-                        //     // console.log('parseJson.oobj.orderName[' + i + '] =' + parseJson[i].oobj.orderName);
-                        //     if (error) {
-                        //         throw error;
-                        //     }
-                        // });
                         break;
 
                     }
                 }
             } else {
                 parseJson.push({ onumber: orderNumber, oobj: orderobj });
-                // dataEmpty =true;
             }
 
 
-            // console.log('parseJson.onumber =' +parseJson.onumber);
-
-
-
-
-            // parseJson.push({onumber:orderNumber, oobj: orderobj});
-
-
-            fs.writeFile(__dirname + '/data/order.json', JSON.stringify(parseJson), function (err) {
+            fs.writeFile( './data/order.json', JSON.stringify(parseJson), function (err) {
                 if (err) throw err;
             });
 
         });
 
 
-        // orderAllDb.findOne({ onumber: orderNumber }).then(function (u) {
-        //     console.log('findOne='+u); // undefined, because we don't have a user with username 'xx'           
-        //     if(u==undefined||u==null||u==""){
-        //         console.log('dataEmpty='+dataEmpty);
-        //         dataEmpty = true;
-        //     }            
-        // });
-        // if (dataEmpty) {
-        //     console.log('insert=');
-        //     orderAllDb.insert({ onumber: orderNumber, oobj: orderobj }).then(function (u) {
-        //         console.log(u); // print user, with a auto generate uuid
-        //     });
-        // }else{
-        // console.log('update=');
-        // orderAllDb.update({ onumber: orderNumber, oobj: orderobj }).then(function (u) {
-        //     console.log(u); // print user, with a auto generate uuid
-        // });
-        // }
-
-
-
-
-
-        // collection.insert({
-        //     "orderNumber": orderNumber,
-        //     "objorder": objorder
-        // });
+       
 
         console.log('Upload to => ' + __dirname + '/uploads' + '/' + phone + '.jpg');
         console.log('done');
 
-        // orderAllDb.find({}).then(function(us){
-        //     console.log(us.length); 
-        //     console.log(us); // an array with one object
-        // });
 
     });
-
-    // console.log('data=' + data_url);
-    // var buffer = new Buffer(data_url, "base64");
-    // console.log('data2=' + buffer);          
-
-    // fs.writeFile("arghhhh.jpg", new Buffer(data_url, "base64"), function (err) {
-    //     res.send('success');
-    //     console.log('done');
-    // });
 
 });
 
